72c67c3cf0689e62a2702bf62a95c1e035613709
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2022-02-12 00:55:23 +0500

    home design

---
+++ js/home.js
@@ -0,0 +1,20 @@
+let elSearchForm = document.querySelector(".home_form");
+let elSearchInput = document.querySelector(".search_input");
+let elDarkModeBtn = document.querySelector(".dark_mode");
+let elLogoutBtn = document.querySelector(".log_out");
+let elNumberBooks = document.querySelector(".result_books_number");
+let elOrderByBtn = document.querySelector(".order_by_btn");
+let elBookmarkList = document.querySelector(".books_bookmark_list");
+let elBooksList = document.querySelector(".books_list");
+let elModal = document.querySelector(".modal");
+let elModalBookName = document.querySelector(".modal_book_name");
+let elModalCloseBtn = document.querySelector(".modal_close_btn");
+let elModalImg = document.querySelector(".modal_book_img");
+let elModalDesc = document.querySelector(".modal_desc");
+let elModalItemInfo = document.querySelector(".modal_item_info");
+let elModalItemDesc = document.querySelector(".modal_item_desc");
+let elModalReadBtn = document.querySelector(".modal_read_btn");
+let elOverly = document.querySelector(".overly");
+//................................................................
+
+const renderBooks = function (arr, place) {};
